b08ff5c368ae0ce61241d8d7205e88e5c8e62541
Łukasz Sypniewski <l.sypniewski@gmail.com>
2018-02-18 23:19:04 +0100

    Initial commit. TODO: Implement GUI, cancelling scheduled action, implement methods also for Func and Func<>.

--- WindowsSleepOnTime/MainWindow.xaml.cs
+++ WindowsSleepOnTime/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RunCodeAtTime;
 
 namespace WindowsSleepOnTime
 {
@@ -23,6 +25,31 @@ namespace WindowsSleepOnTime
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Action<string> action;
+        Action actionWithoutParameters;
+
+        public void ShowMessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+        public void ShowMessageWithoutParam()
+        {
+            MessageBox.Show("Bez parametru");
+        }
+
+        [DllImport("Powrprof.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool SetSuspendState(bool hiberate, bool forceCritical, bool disableWakeEvent);
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            action = new Action<string>(ShowMessage);
+            actionWithoutParameters = new Action(ShowMessageWithoutParam);
+            ScheduleRunner sr = new ScheduleRunner();
+            sr.runCodeAt(DateTime.Now.AddSeconds(2.0), actionWithoutParameters, null);
+            sr.runCodeAt<string>(DateTime.Now.AddSeconds(2.0), action, "Testowa wiadomosc", null);
+            //SetSuspendState(false, true, true);
         }
     }
 }
